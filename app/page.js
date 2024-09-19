f9827df930eee13307db455693b5d9cb6c89cba2
kishorebarath011099@gmail.com <kishorebarath011099@gmail.com>
2024-09-19 11:31:08 +0530

    seo add for homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
+import React from "react"
 import HomeComponents from "@/components/HomeComponents"
+import Seo from "@/components/SeoComponents/Seo"
+import { usePathname } from "next/navigation"
 
-export default function Home() {
+const Page = () => {
+  const pathname = usePathname()
+  const title = "CSI"
+  const description =
+    "Catalysing Social Impact [CSI] is a living forum hosted by the Catalyst Group for thinkers, doers, and investors who come together to ignite positive changes in the field of social impact through strategic initiatives."
+  const path = `${pathname}` // Corrected template string usage
+  const metaImage = ""
+
   return (
-    <>
+    <div>
+      <Seo
+        title={title}
+        description={description}
+        path={path}
+        metaImage={metaImage}
+      />
       <HomeComponents />
-    </>
+    </div>
   )
 }
+
+export default Page
--- components/HomeComponents/index.jsx
+++ components/HomeComponents/index.jsx
@@ -1,22 +1,10 @@
 import React from "react"
 import Homepage from "./Homepage"
 import Aboutpage from "./Aboutpage"
-import Seo from "@/components/SeoComponents/Seo"
 
 const index = () => {
-  const title = "CSI"
-  const description =
-    " Catalysing Social Impact [CSI] is a living forum hosted by the Catalyst Group for thinkers, doers, and investors who come together to ignite positive changes in the field of social impact through strategic initiatives."
-  const path = "csi-react-2024"
-  const metaImage = ""
   return (
     <div>
-      <Seo
-        title={title}
-        description={description}
-        path={path}
-        metaImage={metaImage}
-      />
       <Homepage />
       <Aboutpage />
     </div>
