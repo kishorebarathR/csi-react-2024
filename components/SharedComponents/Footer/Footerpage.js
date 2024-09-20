0b5f916b1dbc3211734246c5bdf7835ff676a94f
kishorebarath011099@gmail.com <kishorebarath011099@gmail.com>
2024-09-20 10:46:43 +0530

    footer page responsive done

--- components/SharedComponents/Footer/Footerpage.js
+++ components/SharedComponents/Footer/Footerpage.js
@@ -1,46 +1,68 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
 
 const Footerpage = () => {
   return (
     <>
-      {/* <!-- Main container --> */}
-      <div className="bg-[#7E3F98] flex flex-wrap justify-between items-center border-b border-white-500 p-5">
-        {/* <!-- Left side with image --> */}
-        <div className="p-5 flex items-center justify-center w-full md:w-auto">
-          <img
-            src="/images/logo-light (1).png"
-            alt="image description"
-            className="w-10/12 md:w-auto"
-            height={400}
-            width={400}
-          />
-        </div>
+    {/* DESKTOP VIEW */}
+<div class="bg-[#7E3F98] border-b border-white-500 pt-5 flex items-center justify-between hidden md:flex">
+  {/* Left side with image */}
+  <div class="w-1/2">
+    <Image
+      src="/images/logo-light (1).png"
+      alt="image description"
+      class="w-10/12 md:w-auto"
+      height={400}
+      width={400}
+    />
+  </div>
 
-        {/* <!-- Right side with text --> */}
-        <div className="flex-1 flex justify-center md:justify-end text-white md:text-right lg:pb-8 ps-8 lg:ps-0 p-5">
-          <a
-            href="mailto:csi@catalysts.org"
-            className="hover:text-green-600 text-md"
-          >
-            csi2014@catalysts.org
-          </a>
-        </div>
-      </div>
+  {/* Right side with text */}
+  <div class="flex-1 text-white pb-5 p-5 md:text-end">
+    <a
+      href="mailto:csi@catalysts.org"
+      class="hover:text-green-600 text-md"
+    >
+      csi2014@catalysts.org
+    </a>
+  </div>
+</div>
 
-      {/* <!-- Navigation section --> */}
-      <div className="bg-[#7E3F98] px-10">
-        <nav>
-          <div className="max-w-screen-xl mx-auto">
-            <div className="flex flex-col md:flex-row justify-between items-center">
-              {/* <!-- Section 1 --> */}
-              <div className="w-full md:w-auto mb-4 md:mb-0">
-                <ul className="font-medium flex flex-col md:flex-row md:space-x-8 items-center">
-                  <li>
+{/* MOBILE VIEW */}
+<div class="bg-[#7E3F98] border-b border-white-500 pt-5 flex flex-col md:flex-row items-center md:justify-between md:hidden">
+  {/* Left side with image */}
+  <div class="w-full md:w-1/2 flex justify-center md:justify-start">
+    <Image
+      src="/images/logo-light (1).png"
+      alt="image description"
+      class="w-10/12 md:w-auto"
+      height={400}
+      width={400}
+    />
+  </div>
+
+  {/* Right side with text */}
+  <div class="w-full text-white pb-5 p-5 text-center md:text-end">
+    <a
+      href="mailto:csi@catalysts.org"
+      class="hover:text-green-600 text-md"
+    >
+      csi2014@catalysts.org
+    </a>
+  </div>
+</div>
+
+      <div className="bg-[#7E3F98] px-10 text-center ">
+        <nav className="">
+          <div className="max-w-screen-xl mx-auto  violet">
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+              <div className="w-full md:w-auto mb-4 md:mb-0 lg:-mt-16 ">
+                <ul className="font-medium flex flex-col md:flex-row md:space-x-8">
+                  <li className="violet">
                     <Link
                       href="/"
-                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent hover:text-green-600"
+                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
                       aria-current="page"
                     >
                       Home
@@ -48,14 +70,12 @@ const Footerpage = () => {
                   </li>
                 </ul>
               </div>
-
-              {/* <!-- Section 2 --> */}
-              <div className="w-full md:w-auto mb-4 md:mb-0">
-                <ul className="font-medium flex flex-col md:flex-row md:space-x-8 items-center">
-                  <li>
+              <div className="w-full md:w-auto mb-4 md:mb-0 lg:-mt-16">
+                <ul className="font-medium flex flex-col md:flex-row md:space-x-8">
+                  <li className="violet">
                     <Link
                       href="csi-2019"
-                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent hover:text-green-600"
+                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
                       aria-current="page"
                     >
                       CSI 2019
@@ -63,57 +83,59 @@ const Footerpage = () => {
                   </li>
                 </ul>
               </div>
-
-              {/* <!-- Section 3 --> */}
-              <div className="w-full md:w-auto mb-4 md:mb-0 text-center">
-                <ul className="font-medium flex flex-col md:flex-row md:space-x-8 items-center">
-                  <li>
-                    <p className="block py-2 px-3 text-md text-white hover:text-green-600">
+              <div className="w-full md:w-auto mb-4 md:mb-0  lg:mt-6 pb-4">
+                <ul className="font-medium flex flex-col md:flex-row md:space-x-8">
+                  <li className="violet">
+                    <p
+                      className="block py-2 px-3 mt-1 text-md text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
+                    >
                       CSI 2024
                     </p>
                     <Link
                       href="csi-2024"
-                      className="block py-2 px-3 text-xs text-white hover:text-green-600"
+                      className="block py-2 px-3 lg:ms-1 mt-1 text-sm text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
                     >
                       Overview
                     </Link>
                     <Link
                       href="csi-agenda"
-                      className="block py-2 px-3 text-xs text-white hover:text-green-600"
+                      className="block py-2 px-3 lg:ms-2 mt-1 text-sm text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
                     >
                       Agenda
                     </Link>
                     <Link
                       href="csi-2024-films"
-                      className="block py-2 px-3 text-xs text-white hover:text-green-600"
+                      className="block py-2 px-3 lg:ms-2 mt-1 text-sm text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
                     >
                       Films
                     </Link>
                   </li>
                 </ul>
               </div>
-
-              {/* <!-- Section 4 --> */}
-              <div className="w-full md:w-auto mb-4 md:mb-0">
-                <ul className="font-medium flex flex-col md:flex-row md:space-x-8 items-center">
-                  <li>
+              <div className="w-full md:w-auto mb-4 md:mb-0 lg:-mt-16">
+                <ul className="font-medium flex flex-col md:flex-row md:space-x-8">
+                  <li className="violet">
                     <Link
                       href="csi-2024-how-to-engage"
-                      className="block py-2 px-3 text-white hover:text-green-600"
+                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
                     >
-                      How to Engage
+                      How to Enagage
                     </Link>
                   </li>
                 </ul>
               </div>
-
-              {/* <!-- Section 5 --> */}
-              <div className="w-full md:w-auto mb-4 md:mb-0">
-                <ul className="font-medium flex flex-col md:flex-row md:space-x-8 items-center">
-                  <li>
+              <div className="w-full md:w-auto lg:-mt-16">
+                <ul className="font-medium flex flex-col md:flex-row md:space-x-8">
+                  <li className="violet">
                     <Link
                       href="https://docs.google.com/forms/d/e/1FAIpQLScwwcEsK3tlih6udYZ1dU2WADxcCf846G6pf4K_qrEfGeYO-A/viewform"
-                      className="block py-2 px-3 text-white hover:text-green-600"
+                      className="block py-2 px-3 text-white md:bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 md:dark:text-violet-500 hover:text-green-600"
+                      aria-current="page"
                     >
                       Subscribe Now
                     </Link>
@@ -124,8 +146,9 @@ const Footerpage = () => {
           </div>
         </nav>
       </div>
+      {/* </div> */}
     </>
-  );
-};
+  )
+}
 
-export default Footerpage;
+export default Footerpage
